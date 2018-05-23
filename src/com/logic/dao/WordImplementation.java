3b907ebe9c210d82b8e6f9d7364452bae412c8db
Ljubisa Savic <ljsavic90@gmail.com>
2018-05-23 10:53:15 +0200

    WordImplementation

--- src/com/logic/dao/WordImplementation.java
+++ src/com/logic/dao/WordImplementation.java
@@ -13,24 +13,97 @@ public class WordImplementation implements WordInterface {
 	Connection connection = ConnectionManager.getInstance().getConnection();
 
 	ArrayList<Word> list = new ArrayList<>();
-	
-	//** dodaje rijeè u bazu (admin funkcija)
-		public boolean addWord(Word word) throws SQLException{
+
+	// ** dodaje rijeÃ¨ u bazu (admin funkcija)
+	public boolean addWord(Word word) throws SQLException {
+
+		String query = "INSERT INTO hangman.word(word_id, word_category, word) VALUES (?, ?, ?)";
+		if (!existWord(word)) {
+			try (PreparedStatement stm = conn.prepareStatement(query)) {
+				stm.setInt(1, word.getWord_id());
+				stm.setInt(2, word.getWord_category());
+				stm.setString(3, word.getWord());
+
+				stm.executeUpdate();
+			}
+			return true;
+		} else {
 			return false;
 		}
+	}
 
-		//**  èita random rijeè iz baze (za svrhu igre)
-		public Word readWord() throws SQLException{
-			return null;
+	private static boolean existWord(Word word) {
+		String query = "SELECT * FROM hangman.word WHERE word_id = ?";
+		ResultSet rs = null;
+
+		try (PreparedStatement stm = conn.prepareStatement(query)) {
+			stm.setInt(1, word.getWord_id());
+			rs = stm.executeQuery();
+			if (rs.next()) {
+				return true;
+			} else {
+				return false;
+			}
 		}
-		
-		//** uzima sve rijeèi iz kategorije ciji je id = cat_id i stavlja ih u listu
-		public ArrayList<Word> getAllWords(int cat_id) throws SQLException{
-			return list;
+
+		return true;
+	}
+
+	// ** Ã¨ita random rijeÃ¨ iz baze (za svrhu igre)
+	public Word readWord() throws SQLException {
+		String query = "SELECT * FROM hangman.word ORDER BY RAND() LIMIT 1";
+		ResultSet rs = null;
+		Word word = null;
+		try (PreparedStatement stm = conn.prepareStatement(query)) {
+
+			rs = stm.executeQuery();
+			word.setWord(rs.getString("word"));
+
 		}
-		
-		//**  uzima iz baze sve rijeèi i stavlja ih u listu
-		public ArrayList<Word> getAllWords() throws SQLException{
-			return list;
+
+		return word;
+	}
+
+	// ** uzima sve rijeÃ¨i iz kategorije ciji je id = cat_id i stavlja ih u
+	// listu
+	public ArrayList<Word> getAllWords(int cat_id) throws SQLException {
+		ArrayList<Word> list = new ArrayList<>();
+
+		String query = "SELECT * FROM hangman.word WHERE word_category = ?";
+		ResultSet rs = null;
+		Word word = null;
+		try (PreparedStatement stm = conn.prepareStatement(query)) {
+			rs = stm.executeQuery();
+
+			while (rs.next()) {
+
+				word.setWord(rs.getString("word"));
+				list.add(word);
+			}
 		}
+
+		return list;
+	}
+
+	// **uzima iz baze sve rijeci i stavlja ih u listu
+	public ArrayList<Word> getAllWords() throws SQLException {
+
+		ArrayList<Word> list = new ArrayList<>();
+		String query = "SELECT * FROM hangman.word";
+		ResultSet rs = null;
+		Word word = null;
+		try (PreparedStatement stm = conn.prepareStatement(query)) {
+			rs = stm.executeQuery();
+
+			while (rs.next()) {
+
+				word.setWord(rs.getString("word"));
+				word.setWord_category(rs.getInt("word_category"));
+				list.add(word);
+			}
+		}
+
+		return list;
+	}
+
 }
